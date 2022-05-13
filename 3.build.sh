d86866f0c41bc43e5eef0f76e48728dc63b77941
hermes3306 <joonho.park@hotmail.com>
2022-05-13 11:12:26 +0900

    build ok

--- 3.build.sh
+++ 3.build.sh
@@ -1,4 +1,6 @@
-./postgre9.3/configure --with-perl --with-python --prefix=./pgsql9.3
-./postgre9.3/make
-./postgre9.3/make all
-./postgre9.3/make install
+cd ./postgres9.3
+./configure --with-perl --with-python --prefix=`pwd`/../pgsql93
+make 	
+make all 
+make install 
+cd ..
