04df3c13a6760b3b3519476d5273b8db6575300f
hermes3306 <joonho.park@hotmail.com>
2022-05-13 10:54:00 +0900

    compile

---
+++ 1.cl.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/postgres/postgres.git --branch REL9_3_STABLE postgres9.3
---
+++ 3.build.sh
@@ -0,0 +1,4 @@
+./postgre9.3/configure --with-perl --with-python --prefix=./pgsql9.3
+./postgre9.3/make
+./postgre9.3/make all
+./postgre9.3/make install
