3482419b82a53110d52ba539c21fcfdf02c5c8c0
joonhopark2021 <6ave54street@gmail.com>
2022-05-16 13:25:52 +0900

    pkg

---
+++ A.pkg.sh
@@ -0,0 +1,15 @@
+#sudo yum install postgresql-server postgresql-contrib
+#sudo yum install postgresql-devel
+
+#sudo yum install python3 python3-devel 
+#sudo yum install python3-pip
+#pip3 install psycopg2
+#pip install psycopg2-binary
+
+#sudo postgresql-setup initdb
+#sudo systemctl start postgresql
+#sudo systemctl enable postgresql
+#sudo passwd postgres
+su - postgres
+
+
