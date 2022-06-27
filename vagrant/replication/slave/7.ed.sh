55a86de84ad640417b7bf98db1ae6c7bc3fc8d7f
hermes3306 <joonho.park@hotmail.com>
2022-06-27 14:38:14 +0000

    slave

---
+++ vagrant/replication/slave/7.ed.sh
@@ -0,0 +1,13 @@
+echo 
+echo
+cat postgresql.conf.rep
+echo 
+echo
+echo "copy the contents and past in the file....."
+echo
+echo
+echo
+echo
+read line
+sudo vi /var/lib/pgsql/data/postgresql.conf
+
