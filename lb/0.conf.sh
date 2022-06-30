a14ba3c393f2f46fe93407babaf47a667ba40d75
hermes3306 <joonho.park@hotmail.com>
2022-06-30 02:10:33 +0000

    x

---
+++ lb/0.conf.sh
@@ -0,0 +1,24 @@
+echo 
+echo
+echo 
+echo
+echo "edit /var/lib/pgsql/data/pg_hba.conf ...."
+echo
+echo
+echo
+echo
+read line
+sudo vi /var/lib/pgsql/data/pg_hba.conf
+
+echo 
+echo
+echo 
+echo
+echo "edit /var/lib/pgsql/data/postgresql.conf ...."
+echo
+echo
+echo
+echo
+read line
+sudo vi /var/lib/pgsql/data/postgresql.conf
+
