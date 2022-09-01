001b735449a602528e938383bb40632f8ef8c641
mason-lee-alti <mason.lee@altibase.com>
2022-09-01 09:33:38 +0900

    x

---
+++ APT/neo4j/start.sh
@@ -0,0 +1,7 @@
+sudo systemctl daemon-reload
+sudo systemctl stop neo4j
+sudo systemctl disable neo4j
+sudo systemctl enable neo4j
+sudo systemctl start neo4j
+
+sudo update-alternatives --config java
